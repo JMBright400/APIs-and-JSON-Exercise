117f195eff5811a224cc276767493ed1dbd44d52
John Bright <jmbright400@gmail.com>
2023-11-10 10:55:00 -0600

    Kanye and Ron talk together. You can also find the temperature of your city.

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
-namespace APIsAndJSON
+namespace APIsAndJSON   
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        public static void GetTemp()
+        {
+            var apiKeyObj = File.ReadAllText("appsettings.json");
+
+            var apiKey =JObject.Parse(apiKeyObj).GetValue("apiKey".ToString());
+
+            Console.Write("Enter ZIP Code: ");
+
+            var zip = Console.ReadLine();
+
+            var url = $"http://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={apiKey}&units=imperial";
+
+            var client = new HttpClient();
+
+            var jtext = client.GetStringAsync(url).Result;
+
+            var weatherObj = JObject.Parse(jtext);
+
+            Console.WriteLine($"Temp: {weatherObj["main"]["temp"]}");
+        }
     }
 }
+
+
+
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static void Conversation()
+        {
+            var client = new HttpClient();
+
+            for(int i = 0; i < 5; i++) 
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"Ron says:\n{GetRonQuote(client)}\n");
+                Thread.Sleep(2000);
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.WriteLine($"Kanye says:\n{GetKanyeQuote(client)}\n");
+                Thread.Sleep(2000);
+            }
+
+            Console.ResetColor();
+        }
+        private static string GetRonQuote(HttpClient client)
+        {
+            var jtext = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+
+            var quote = JArray.Parse(jtext).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+            return quote;
+        }
+        private static string GetKanyeQuote(HttpClient client)
+        {
+            var jtext = client.GetStringAsync("https://api.kanye.rest/").Result;
+            
+            var quote = JObject.Parse(jtext).GetValue("quote").ToString();
+
+            return quote;
+
+        }
     }
 }
